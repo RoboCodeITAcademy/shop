908c2b059fbe66d1ec5df312c2692c99a0015705
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-23 10:33:59 +0500

    add cart app

--- shop/urls.py
+++ shop/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('main.urls', namespace='main')),
+    path("cart/", include('cart.urls', namespace='cart')),
 ]
 
 if settings.DEBUG:
